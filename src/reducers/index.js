806f215fb8ed20723e660e9b8f9811ac6da17f7c
Zoe <zoekharvey@gmail.com>
2019-10-31 15:51:14 -0600

    Add get player into combined reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { teams } from './teams';
 import { errorMsg } from './hasError';
 import { isLoading } from './isLoading';
 import { standings } from './standings';
-import { roster } from './roster'
+import { roster } from './roster';
+import { player } from './player';
 
 export const rootReducer = combineReducers({
     teams,
     isLoading,
     errorMsg,
     standings,
-    roster
+    roster,
+    player
 })
