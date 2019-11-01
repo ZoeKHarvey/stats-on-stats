46d7f7fa271882d386c5c95c35ccf95d05f05a05
Zoe <zoekharvey@gmail.com>
2019-11-01 15:41:15 -0600

    Add all logos

--- src/Components/NavLogos/NavLogos.js
+++ src/Components/NavLogos/NavLogos.js
@@ -1,17 +1,77 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavLogos.scss'
 // import { connect } from 'react-redux';
 import flyers from '../../images/flyers.png';
 import blues from '../../images/blues.png';
+import bruins from '../../images/bruins.png';
+import rangers from '../../images/rangers.png';
+import canadiens from '../../images/canadiens.png';
+import redwings from '../../images/redwings.png';
+import leafs from '../../images/leafs.png';
+import blackhawks from '../../images/blackhawks.jpg';
+import pens from '../../images/pens.png';
+import kings from '../../images/kings.png';
+import sabres from '../../images/sabres.png';
+import canucks from '../../images/canucks.png';
+import islanders from '../../images/islanders.png';
+import capitals from '../../images/capitals.png';
+import oilers from '../../images/oilers.png';
+import sharks from '../../images/sharks.png';
+import senators from '../../images/senators.jpg';
+import lightning from '../../images/lightning.png';
+import panthers from '../../images/panthers.jpeg';
+import ducks from '../../images/ducks.png';
+import devils from '../../images/devils.png';
+import flames from '../../images/flames.png';
+import avalanche from '../../images/avalanche.png';
+import stars from '../../images/stars.png';
+import coyotes from '../../images/coyotes.png';
+import wild from '../../images/wild.png';
+import predators from '../../images/predators.png';
+import bluejackets from '../../images/bluejackets.png';
+import jets from '../../images/jets.png';
+import hurricanes from '../../images/hurricanes.png';
+import knights from '../../images/golden-knights.png';
 
 
 export const NavLogo = () => {
 
 return(
   <nav>
-    
+    <img src={bruins} alt="flyers logo link to team page"/>
+    <img src={rangers} alt="flyers logo link to team page"/>
+    <img src={canadiens} alt="flyers logo link to team page"/>
+    <img src={redwings} alt="flyers logo link to team page"/>
+    <img src={leafs} alt="flyers logo link to team page"/>
+    <img src={blackhawks} alt="flyers logo link to team page"/>
+
     <img src={flyers} alt="flyers logo link to team page"/>
     <img src={blues} alt="blues logo link to team page"/>
+    <img src={pens} alt="flyers logo link to team page"/>
+    <img src={kings} alt="flyers logo link to team page"/>
+
+    <img src={sabres} alt="flyers logo link to team page"/>
+    <img src={canucks} alt="flyers logo link to team page"/>
+    <img src={islanders} alt="flyers logo link to team page"/>
+    <img src={capitals} alt="flyers logo link to team page"/>
+    <img src={oilers} alt="flyers logo link to team page"/>
+    <img src={sharks} alt="flyers logo link to team page"/>
+    <img src={senators} alt="flyers logo link to team page"/>
+    <img src={lightning} alt="flyers logo link to team page"/>
+    <img src={panthers} alt="flyers logo link to team page"/>
+    <img src={ducks} alt="flyers logo link to team page"/>
+    <img src={devils} alt="flyers logo link to team page"/>
+    <img src={flames} alt="flyers logo link to team page"/>
+    <img src={avalanche} alt="flyers logo link to team page"/>
+    <img src={stars} alt="flyers logo link to team page"/>
+    <img src={coyotes} alt="flyers logo link to team page"/>
+    <img src={wild} alt="flyers logo link to team page"/>
+    <img src={predators} alt="flyers logo link to team page"/>
+    <img src={bluejackets} alt="flyers logo link to team page"/>
+    <img src={jets} alt="flyers logo link to team page"/>
+    <img src={hurricanes} alt="flyers logo link to team page"/>
+    <img src={knights} alt="flyers logo link to team page"/>
   </nav>
 )
 
