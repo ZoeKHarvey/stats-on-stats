293477373c2a4ae39030d400f193db2a4587eb82
Zoe <zoekharvey@gmail.com>
2019-11-01 15:00:41 -0600

    Add blues logo

--- src/Components/NavLogos/NavLogos.js
+++ src/Components/NavLogos/NavLogos.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 // import { connect } from 'react-redux';
-import flyers from '../../images/flyers.png'
+import flyers from '../../images/flyers.png';
+import blues from '../../images/blues.png';
+
 
 export const NavLogo = () => {
 
 return(
   <nav>
+    
     <img src={flyers} alt="flyers logo link to team page"/>
+    <img src={blues} alt="blues logo link to team page"/>
   </nav>
 )
 
