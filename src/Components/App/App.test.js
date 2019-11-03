36007536befe7a346638ebcab19e01ee16a31161
Zoe <zoekharvey@gmail.com>
2019-11-03 10:41:08 -0700

    Add state to props and dispatch to props tests

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -16,5 +16,37 @@ describe('App', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+});
 
+describe('mapStateToProps', () => {
+  it('should return an object with', () => {
+    let teams;
+    const mockState = {
+      teams
+    };
+
+    const expected = {
+      teams
+    };
+
+    const mappedProps = mapStateToProps(mockState);
+
+    expect(mappedProps).toEqual(expected)
+  });
+
+  describe('mapDispatchToProps', () => {
+    let mockDispatch;
+
+    beforeEach(() => {
+      mockDispatch = jest.fn();
+    });
+
+    it('calls dispatch with a getTeams action', () => {
+      const actionToDispatch = getTeams([{}, {}]);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.getTeams([{}, {}]);
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+    })
+  })
 })
