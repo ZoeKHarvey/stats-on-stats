0d72117a9f416bf43c61ebaefe1b4c5641fd5706
Zoe <zoekharvey@gmail.com>
2019-11-02 23:25:41 -0600

    Add test for fetch rosters

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -22,7 +22,19 @@ describe('getTeams', () => {
   it('should return an array of players', () => {
     expect(fetchTeams()).resolves.toEqual(mockResponse)
   })
+})
 
+describe('fetchRoster', () => {
+  const mockResponse = [{}, {}, {}];
+  let id = 111111;
+  const url = `https://statsapi.web.nhl.com/api/v1/teams/${id}?expand=team.roster`;
 
-
+  it('should call fetch with the correct url', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      })
+    })
+  })
 })
