938f0167dc28e0e225934f5c52d0f0e52837af25
Zoe <zoekharvey@gmail.com>
2019-11-04 09:41:18 -0700

    Fix roster loading error

--- src/Components/RosterContainer/RosterContainer.js
+++ src/Components/RosterContainer/RosterContainer.js
@@ -2,93 +2,31 @@ import React, { Component } from 'react';
 import TeamCard from '../TeamCard/TeamCard'
 import { connect } from 'react-redux';
 import RosterCard from '../RosterCard/RosterCard'
-import './RosterContainer.scss';
-import { fetchRoster, fetchTeamSchedule } from '../../apiCalls'
+import './RosterContainer.scss'
 
-class RosterContainer extends Component {
-
-  componentDidMount = async () => {
-    const {} = this.props;
-    try {
-      this.getSingleRoster();
-      this.getSingleTeamSchedule()
-      // fetchPlayerProjections()
-    } catch (error) {
-      console.log('error')
-    }
-  }
-
-
-    getSingleRoster = async(id) =>{
-      const { getRoster } = this.props;
-      try {
-        const roster = await fetchRoster(id);
-        getRoster(roster)
-      } catch(error) {
-        console.log('error')
-      }
-    }
-  
-    getSingleTeamSchedule = async(id) => {
-      const { getTeamSchedule } = this.props;
-      try {
-        const schedule = await fetchTeamSchedule(id);
-        console.log('schedule in function', schedule)
-        getTeamSchedule(this.cleanUpSchedule(schedule))
-      } catch(error) {
-        return 'No Games Today'
-      }
-    }
-  
-    cleanUpSchedule = (schedule) => {
-      return {
-        away: {
-        awayTeam: schedule.teams.away.team.name,
-        awayWins: schedule.teams.away.leagueRecord.wins,
-        awayLosses: schedule.teams.away.leagueRecord.losses,
-        awayOT: schedule.teams.away.leagueRecord.ot },
-        home: {
-          homeTeam: schedule.teams.home.team.name,
-          homeWins: schedule.teams.home.leagueRecord.wins,
-          homeLosses: schedule.teams.home.leagueRecord.losses,
-          homeOT: schedule.teams.home.leagueRecord.ot
-      }
-    }
-  }
-  
-    // handleDetails = (e, id) => {
-    //   this.getSingleRoster(e, id)
-    //   this.getSingleTeamSchedule(e, id)
-    // }
-
-populateCards = () => {
-  const {roster, getSinglePlayer, teamSchedule } = this.props
-  roster.map((roster) => {
+export const RosterContainer = ({roster, getSinglePlayer, teamSchedule}) => {
+  console.log('roster in container-->', roster)
+  const singleRoster = roster.map((roster) => {
     return <RosterCard {...roster}
                         getSinglePlayer = { getSinglePlayer } 
                         teamSchedule = { teamSchedule }
                         />
 })
-}
 
+  
 
-
-    render() {
     return(
       <section className="section-rostercontainer">
         <div className="roster__container--game">
         <h2 className="roster--h2--game">Today's Game</h2>
-
         {/* <div className="roster__div--schedule">
           <div className="roster__div--home">
             <h3>Home Team</h3>
-
           <h3 className="roster__h3--home">{ teamSchedule.home.homeTeam }</h3>
         <h4>Wins { teamSchedule.home.homeWins }</h4>
         <h4>Losses{ teamSchedule.home.homeLosses }</h4>
         <h4>OTL: { teamSchedule.home.homeOT}</h4>
         </div>
-
         <div className="roster__div--away">
           <h3>Away Team</h3>
         <h3 className="roster__h3--away">{ teamSchedule.away.awayTeam }</h3>
@@ -99,12 +37,10 @@ populateCards = () => {
         </div> */}
         <h1 className="roster__h1--roster">Roster</h1>
         </div>
-        {this.populateCards}
+        {singleRoster}
       </section>
     )
 }
-}
-
 
 export const mapStateToProps = (state) => ({
   teams: state.teams,
--- src/Components/TeamCard/TeamCard.js
+++ src/Components/TeamCard/TeamCard.js
@@ -10,11 +10,59 @@ import { Route } from 'react-router-dom';
 
 export class TeamCard extends Component {
 
+  getSingleRoster = async(id) =>{
+    const { getRoster } = this.props;
+    try {
+      const roster = await fetchRoster(id);
+      getRoster(roster)
+    } catch(error) {
+      console.log('error')
+    }
+  }
+
+  getSingleTeamSchedule = async(id) => {
+    const { getTeamSchedule } = this.props;
+    try {
+      const schedule = await fetchTeamSchedule(id);
+      console.log('schedule in function', schedule)
+      getTeamSchedule(this.cleanUpSchedule(schedule))
+    } catch(error) {
+      return 'No Games Today'
+    }
+  }
+
+  // favoriteTeam = (e, id) => {
+  //   const { favoriteTeams } = this.props
+  //   favoriteTeams(id)
+  // }
+
+  cleanUpSchedule = (schedule) => {
+    return {
+      away: {
+      awayTeam: schedule.teams.away.team.name,
+      awayWins: schedule.teams.away.leagueRecord.wins,
+      awayLosses: schedule.teams.away.leagueRecord.losses,
+      awayOT: schedule.teams.away.leagueRecord.ot },
+      home: {
+        homeTeam: schedule.teams.home.team.name,
+        homeWins: schedule.teams.home.leagueRecord.wins,
+        homeLosses: schedule.teams.home.leagueRecord.losses,
+        homeOT: schedule.teams.home.leagueRecord.ot
+    }
+  }
+}
+
+  handleDetails = (e, id) => {
+    this.getSingleRoster(id)
+    this.getSingleTeamSchedule(id)
+    // this.favoriteTeam(e, id)
+  }
+
   render() {
-    const {id, name, venue, firstYearOfPlay, division, conference, officialSiteUrl, teamSchedule } = this.props;
-    // const teamSchedError = teamSchedule.home.homeTeam || 'No Games Today'
+    const {id, name, venue, firstYearOfPlay, division, conference, officialSiteUrl } = this.props;
+    // const teamSchedError = teamSchedule || 'No Games Today'
+    console.log('IDDDDD', id)
       return (
-        
          <section className="team-card" > 
           <h2>{name}</h2>
           <a href={officialSiteUrl}>Official Site</a>
@@ -24,8 +72,7 @@ export class TeamCard extends Component {
           <h4>Division: {division.name}</h4>
           <h4>Conference: {conference.name}</h4>
           <Link to='/roster' >
-            <button >Show Details</button>
-            {/* {teamSchedError} */}
+            <button onMouseEnter={(e) => this.handleDetails(e, id)}>Show Details</button>
           </Link>
         </section>
       )
