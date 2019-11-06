4be9773585d35b74b4375c61b2a69b64d7d8bd30
Zoe <zoekharvey@gmail.com>
2019-11-06 13:43:09 -0700

    Fix api tests finally

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,54 +1,56 @@
 export const fetchTeams = async() => {
   const response = await fetch('https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster');
-  const data = await response.json();
   if (!response.ok) {
     throw new Error('There was an error getting the teams.');
   }
+  const data = await response.json();
   return data.teams
 }
 
 
 export const fetchRoster = async(id) => {
   const response = await fetch(`https://statsapi.web.nhl.com/api/v1/teams/${id}?expand=team.roster`)
-  const data = await response.json();
   if (!response.ok) {
     throw new Error('There was an error getting the roster.');
   }
+  const data = await response.json();
   return data.teams[0].roster.roster
 }
 
 export const fetchPlayer = async(id) => {
   const response = await fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}`)
-  const data = await response.json();
   if (!response.ok) {
     throw new Error('There was an error getting this player.');
   }
-  return data.people[0]
+  const data = await response.json();
+  console.log('fetch people', data)
+  return data.people[0];
 }
 
 export const fetchPlayerStats = async(id, year = 20192020) => {
   const response = await fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=statsSingleSeason&season=${year}`)
-  const data = await response.json();
   if (!response.ok) {
     throw new Error('There was an error getting the stats.');
   }
+  const data = await response.json();
   return data.stats[0].splits[0].stat
 }
 
 export const fetchTeamSchedule = async(id) => {
   const response = await fetch(`https://statsapi.web.nhl.com/api/v1/schedule?teamId=${id}`)
-  const data = await response.json();
   if (!response.ok) {
     throw new Error('There was an error getting the schedule.');
   }
+  const data = await response.json();
+  console.log('fetch sched', data)
   return data.dates[0].games[0]
 }
 
 export const fetchPlayerProjections = async(id) => {
   const response = await fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=onPaceRegularSeason&season=20192020`)
-  const data = await response.json();
   if (!response.ok) {
     throw new Error('There was an error getting these projections.');
   }
+  const data = await response.json();
   return data.stats[0].splits[0]
 }
--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -1,208 +1,183 @@
-// import {
-//   fetchTeams,
-//   fetchRoster,
-//   fetchPlayer,
-//   fetchPlayerStats,
-//   fetchTeamSchedule,
-//   fetchPlayerProjections,
-// } from './apiCalls';
+import {
+  fetchTeams,
+  fetchRoster,
+  fetchPlayer,
+  fetchPlayerStats,
+  fetchTeamSchedule,
+  fetchPlayerProjections,
+} from './apiCalls';
 
-// describe('getTeams', () => {
-//   const mockResponse = [{
-//   id: 20,
-//   name:"Calgary Flames",
-//   link:"/api/v1/teams/20",
-//   abbreviation:"CGY",
-//   teamName:"Flames",
-//   locationName:"Calgary",
-//   firstYearOfPlay:"1980",
-//   shortName:"Calgary",
-//   officialSiteUrl:"http://www.calgaryflames.com/",
-//   franchiseId:21,
-//   active:true }]
+describe('getTeams', () => {
+  const mockResponse = {teams: [{name:'flyers'}, {name: 'blues'}, {name:'preds'}]}
 
-//   const url = 'https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster'
+  const url = 'https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster'
 
-//   it('should call fetch with the correct URL', () => {
-//     window.fetch = jest.fn().mockImplementation(() => {
-//       return Promise.resolve({ 
-//         ok: true,
-//         json: () => Promise.resolve(mockResponse)
-//       });
-//     });
+  it('should call fetch with the correct URL', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({ 
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
 
-//     fetchTeams();
+    fetchTeams();
     
-//     expect(window.fetch).toHaveBeenCalledWith(url)
-//   });
+    expect(window.fetch).toHaveBeenCalledWith(url)
+  });
 
-//   it('should return an array of players', () => {
-//     expect(fetchTeams()).resolves.toEqual(mockResponse)
-//   });
-// });
+  it('should return an array of players', () => {
+    expect(fetchTeams()).resolves.toEqual(mockResponse.teams)
+  });
+});
 
-// it('should an error if response is not ok', () => {
-//   window.fetch = jest.fn().mockImplementation(() => {
-//     return Promise.resolve({ 
-//       ok: false
-//     })
-//   })
-//   expect(fetchTeams()).rejects.toEqual(Error('There was an error getting the teams.'))
-// })
+it('should an error if response is not ok', () => {
+  window.fetch = jest.fn().mockImplementation(() => {
+    return Promise.resolve({ 
+      ok: false
+    })
+  })
+  expect(fetchTeams()).rejects.toEqual(Error('There was an error getting the teams.'))
+})
 
-// it('should return an error if promise rejects', () => {
-//   window.fetch = jest.fn().mockImplementation(() => {
-//     return Promise.reject(Error('fetch failed'))
-//   });
+it('should return an error if promise rejects', () => {
+  window.fetch = jest.fn().mockImplementation(() => {
+    return Promise.reject(Error('fetch failed'))
+  });
 
-//   expect(fetchTeams()).rejects.toEqual(Error('fetch failed'));
-// });
+  expect(fetchTeams()).rejects.toEqual(Error('fetch failed'));
+});
 
-// describe('fetchRoster', () => {
-//   let url;
-//   let id;
-//   const mockResponse = [
-//     {name: 'name1', jerseyNumber: 2, position: 'C'}, 
-//     {name: 'name2', jerseyNumber: 3, position: 'D'}, 
-//     {name: 'name3', jerseyNumber: 4, position: 'D'}];
+describe('fetchRoster', () => {
+  let url;
+  let id;
+  const mockResponse = {teams: [{name: 'me', roster: {roster: 'roster'}}, {name: 'dog', roster: {roster: 'roster'}}]}
   
-//   it('should call fetch with the correct url', () => {
-//     id = 8476941;
-//     url = `https://statsapi.web.nhl.com/api/v1/teams/${id}?expand=team.roster`;
-//     window.fetch = jest.fn().mockImplementation(() => {
-//       return Promise.resolve({
-//         ok: true,
-//         json: () => Promise.resolve(mockResponse)
-//       });
-//     });
-//     fetchRoster(id);
-//     expect(window.fetch).toHaveBeenCalledWith(url)
-//   });
+  it('should call fetch with the correct url', () => {
+    id = 8476941;
+    url = `https://statsapi.web.nhl.com/api/v1/teams/${id}?expand=team.roster`;
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+    fetchRoster(id);
+    expect(window.fetch).toHaveBeenCalledWith(url)
+  });
 
-//   it('it should return an error if promise rejects', () => {
-//     window.fetch = jest.fn().mockImplementation(() => {
-//       return Promise.reject(Error('fetch failed'))
-//     });
+  it('it should return an error if promise rejects', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(Error('fetch failed'))
+    });
 
-//     expect(fetchRoster()).rejects.toEqual(Error('fetch failed'));
-//   });
-// });
+    expect(fetchRoster()).rejects.toEqual(Error('fetch failed'));
+  });
+});
 
-// describe('fetchPlayer', () => {
-//   let url;
-//   let id;
-//   const mockResponse = [
-//     {id:8475191,
-//     fullName:"Reilly Smith",
-//     active:true,
-//     captain:false,
-//     height:"6'1",
-//     weight:183,
-//     birthCity:"Mimico",
-//     birthStateProvince:"ON",
-//     birthCountry:"CAN",
-//     currentAge:28,
-//     rosterStatus:"Y",
-//     shootsCatches:"L"}
-//     ]
+describe('fetchPlayer', () => {
+  let url;
+  let id;
+  const mockResponse = {people: [{id: 1}, {id: 2}, {id:8476941}]}
 
-//   it('should call fetchPlayer with the correct url', () => {
-//     id = 8476941;
-//     url = `https://statsapi.web.nhl.com/api/v1/people/${id}`;
-//     window.fetch = jest.fn().mockImplementation(() => {
-//       return Promise.resolve({
-//         ok: true,
-//         json: () => Promise.resolve(mockResponse)
-//       });
-//     });
-//     fetchPlayer(id);
-//     expect(window.fetch).toHaveBeenCalledWith(url)
-//   });
+  it('should call fetchPlayer with the correct url', () => {
+    id = 8476941;
+    url = `https://statsapi.web.nhl.com/api/v1/people/${id}`;
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+    fetchPlayer(id);
+    expect(window.fetch).toHaveBeenCalledWith(url)
+  });
 
-//   it('it should return an error if promise rejects', () => {
-//     window.fetch = jest.fn().mockImplementation(() => {
-//       return Promise.reject(Error('fetch failed'))
-//     });
+  it('it should return an error if promise rejects', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(Error('fetch failed'))
+    });
 
-//     expect(fetchPlayer()).rejects.toEqual(Error('fetch failed'));
-//   });
-// });
+    expect(fetchPlayer()).rejects.toEqual(Error('fetch failed'));
+  });
+});
 
-// describe('fetchPlayerStats', () => {
-//   let url;
-//   let id;
-//   let year;
-//   const mockResponse = [
-//     {assists: 1,
-//     goals: 2,
-//     gamesPlayed: 1,
-//     hits: 3,
-//     pim: 6,
-//     totalPoints: 3,
-//     shootingPercentage: 4}
-//   ]
+describe('fetchPlayerStats', () => {
+  let url;
+  let id;
+  let year;
 
-//   it('should call fetchPlayerStats with the correct url', () => {
-//     id = 8476941;
-//     year = 20192020;
-//     url = `https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=statsSingleSeason&season=${year}`;
-//     window.fetch = jest.fn().mockImplementation(() => {
-//       return Promise.resolve({
-//         ok:true,
-//         json: () => Promise.resolve(mockResponse)
-//       });
-//     });
-//     fetchPlayerStats(id, year);
-//     expect(window.fetch).toHaveBeenCalledWith(url)
-//   });
+  const mockResponse = {
+      stats: [{splits: [{stat: 3}]}]
+    }
+  
+  it('should call fetchPlayerStats with the correct url', () => {
+    id = 8476941;
+    year = 20192020;
+    url = `https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=statsSingleSeason&season=${year}`;
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok:true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+    fetchPlayerStats(id, year);
+    expect(window.fetch).toHaveBeenCalledWith(url)
+  });
 
-//   it('it should return an error if promise rejects', () => {
-//     window.fetch = jest.fn().mockImplementation(() => {
-//       return Promise.reject(Error('fetch failed'))
-//     });
+  it('it should return an error if promise rejects', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(Error('fetch failed'))
+    });
 
-//     expect(fetchPlayerStats()).rejects.toEqual(Error('fetch failed'));
-//   });
+    expect(fetchPlayerStats()).rejects.toEqual(Error('fetch failed'));
+  });
 
-// it('should call fetchTeamSchedule with the correct url', () => {
-//   id = 8476941;
-//   url = `https://statsapi.web.nhl.com/api/v1/schedule?teamId=${id}`;
-//   window.fetch = jest.fn().mockImplementation(() => {
-//     return Promise.resolve({
-//       ok: true,
-//       json: () => Promise.resolve(mockResponse)
-//     });
-//   });
-//   fetchTeamSchedule(id);
-//   expect(window.fetch).toHaveBeenCalledWith(url)
-// });
 
-// it('SAD: should return an error if promise rejects', () => {
-//   window.fetch = jest.fn().mockImplementation(() => {
-//     return Promise.reject(Error('fetch failed'))
-//   });
+it('should call fetchTeamSchedule with the correct url', () => {
+  id = 8476941;
+  url = `https://statsapi.web.nhl.com/api/v1/schedule?teamId=${id}`;
 
-//   expect(fetchTeamSchedule()).rejects.toEqual(Error('fetch failed'));
-// });
+  const mockResponse = {
+    dates: [
+      { games: ['bob']}
+    ]
+  }
+  window.fetch = jest.fn().mockImplementation(() => {
+    return Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve(mockResponse)
+    });
+  });
+  fetchTeamSchedule(id);
+  expect(window.fetch).toHaveBeenCalledWith(url)
+});
 
-// it('should call fetchPlayerProjections', () => {
-//   id= 8476951;
-//   url= `https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=onPaceRegularSeason&season=20192020`;
-//   window.fetch = jest.fn().mockImplementation(() => {
-//     return Promise.resolve({
-//       ok:true,
-//       json: () => Promise.resolve(mockResponse)
-//     });
-//   });
-//   fetchPlayerProjections(id);
-//   expect(window.fetch).toHaveBeenCalledWith(url)
-// });
-// it('it should return an error if promise rejects', () => {
-//   window.fetch = jest.fn().mockImplementation(() => {
-//     return Promise.reject(Error('fetch failed'))
-//   });
+it('SAD: should return an error if promise rejects', () => {
+  window.fetch = jest.fn().mockImplementation(() => {
+    return Promise.reject(Error('fetch failed'))
+  });
 
-//   expect(fetchPlayerProjections()).rejects.toEqual(Error('fetch failed'));
-// });
+  expect(fetchTeamSchedule()).rejects.toEqual(Error('fetch failed'));
+});
 
-// });
+it('should call fetchPlayerProjections', () => {
+  id= 8476951;
+  url= `https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=onPaceRegularSeason&season=20192020`;
+  window.fetch = jest.fn().mockImplementation(() => {
+    return Promise.resolve({
+      ok:true,
+      json: () => Promise.resolve(mockResponse)
+    });
+  });
+  fetchPlayerProjections(id);
+  expect(window.fetch).toHaveBeenCalledWith(url)
+});
+it('it should return an error if promise rejects', () => {
+  window.fetch = jest.fn().mockImplementation(() => {
+    return Promise.reject(Error('fetch failed'))
+  });
+
+  expect(fetchPlayerProjections()).rejects.toEqual(Error('fetch failed'));
+});
+
+});
